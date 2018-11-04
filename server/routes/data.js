e11def3951f010c81042d0f0ea68b7074cecca22
Daley <daley.mottley@hotmail.com>
2018-11-04 17:13:36 +0000

    Create data seed

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,8 +1,21 @@
 var express = require('express');
+var Data = require('./models/Data');
 var router = express.Router();
 
 router.get('/', function(req, res){
   res.send('success');
+});
+
+router.get('/seed', function(req, res) {
+    Data.create({}, function(err, data) {
+        if (err) { 
+            console.log(err)
+            res.status(401).send(err);
+        } else {
+            console.log(data);
+            res.status(200).json(data);
+        }
+    });
 });
 
 module.exports = router;
