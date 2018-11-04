403a0b50eb59099831278621fb53162a3e426d57
Daley <daley.mottley@hotmail.com>
2018-11-04 16:04:59 +0000

    Install mongoose. Install and implement cors

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var cors = require('cors');
+var serverUrl = require("../server-url");
 
 var app = express();
 
@@ -18,11 +20,7 @@ app.set('view engine', 'jade');
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.use(cors({origin: serverUrl + ':8081', }))
 
 app.get('/', routes);
 app.get('/users', user);
